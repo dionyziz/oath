6e5f0bf91c8622ddccb3405d2b641fd15ab9b700
Angel Garcia Cuartero <anhelido@gmail.com>
2013-12-11 17:18:06 +0100

    Spanish translation added

---
+++ lang/es.php
@@ -0,0 +1,48 @@
+<?php
+    return array(
+        I_TITLE => 'Juramento',
+        I_SWEAR => 'Juro,',
+        I_INTRO => 'cumplir de acuerdo a lo mejor
+        
+                    de mi *habilidad* y *juicio*,
+                    
+                    este juramento y este pacto',
+        I_OATH  => array(
+            'Respetaré el *conocimiento* de la gente cuyos pasos sigo (y a aquellos que me enseñen y ayuden, y compartiré de buena gana este conocimiento con los siguientes, así como otros lo han compartido conmigo. Recordaré que
+            el *conocimiento no debe permanecer en secreto* para aquellos con ganas de aprender.)',
+            'Utilizaré la tecnología para el *bien* (y para el *progreso* de la humanidad, libre de todo mal, 
+            para mejorar las vidas de las personas.)',
+            'No usaré mi conocimiento para provecho injusto (financiero o de otro tipo. No afirmaré que el uso de la tecnología
+            es necesario cuando no lo es, sino sólo para solucionar problemas *reales*, libre de toda injusticia intencionada.)',
+            'Recordaré que la tecnología siempre *mejora* (, y nunca dejaré de aprender, y el propósito de mi vida   
+            será una *automejora* continuada.)',
+            'Recordaré que hay *arte* en la ingeniería del software así como tecnología (, y que hay belleza en el código. No
+            me olvidaré de que los programas deben ser escritos para que la *gente* los lea, y sólo ocasionalmente para que las máquinas los ejecuten.)',
+            'Recordaré que los fundamentos de la tecnología yacen en la *ciencia* (, y no olvidaré la contribución de los científicos que apoyaron
+            mi arte; aquellos que desarrollaron las matemáticas, física, química y la ciencia de la computación.)',
+            'No me avergonzaré de decir "No lo sé"(, ni dejaré de requerir a otro ingeniero si es necesario.)',
+            'Cuando por medio de la tecnología obtenga acceso a datos privados, respetaré su *privacidad* (
+            y no abusaré de dicha información obtenida.)',
+            'Recordaré como ingeniero que mi capacidad y poder conlleva una *obligación* (
+            de *crear* y mejorar la vida de mis congéneres.)'
+        ),
+        I_CC => 'Licencia Creative Commons',
+        I_EPILOGUE0 => 'Si no incumplo este juramento,
+        
+                        que se me conceda disfrutar de la *vida* y del *arte*,
+                        
+                        sea *respetado* mientras vivo
+                        
+                        y recordado con *afecto* en adelante.',
+        I_EPILOGUE1 => 'Que actúe para preservar las *mejores tradiciones* de mi vocación
+        
+                        y que experimente el gozo de la *innovación*.
+                        
+                        Y que experimente el *gozo* de ofrecer a mi arte
+                        
+                        nuevos elementos que lo hagan *progresar*.',
+        I_TAKE      => 'Haz el juramento',
+        I_SHARE_FB  => 'Comparte en Facebook',
+        I_SHARE_TW  => 'Comparte en Twitter'
+    );
+?>
