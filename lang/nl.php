1a1acdd73d0191ee3ad29effc5b83bd3fbe7edc9
Sander Marechal <s.marechal@jejik.com>
2013-12-11 14:06:08 +0100

    Add Dutch translation

---
+++ lang/nl.php
@@ -0,0 +1,50 @@
+<?php
+    return array(
+        I_TITLE => 'Eed',
+        I_SWEAR => 'Ik zweer,',
+        I_INTRO => 'naar mijn beste *vermogen* en *inzicht*,
+        
+                    deze eed en deze overeenkomst
+                    
+                    te vervullen',
+        I_OATH  => array(
+            'I zal de *kennis* van de mensen in wiens voetsporen ik treed respecteren (en van hen die mij onderwijzen en helpen,
+            en zal deze kennis delen met hen die mij volgen, als anderen met mij hebben gedeeld. Ik zal onthouden dat
+            *kennis niet geheim moet worden gehouden* van hen die willen leren.)',
+            'Ik zal technologie gebruiken voor *goed* (en voor de vooruitgang van de mensheid, vrij van kwaad, voor de verbetering
+            van het leven van mensen.)',
+            'Ik zal mijn kennis niet gebruiken voor oneerlijk gewin(, financieel of anders. Ik zal niet beweren dat gebruik van
+            technologie noodzakelijk is wanneer dat niet is, maar alleen voor het oplossen van *echte* problemen, vrij van opzettelijk onrecht.)',
+            'Ik zal onthouden dat technologie altijd *verbetert*(, en zal nooit stoppen met leren, en het doel van mijn leven zal
+            continue *zelfverbetering* zijn.)',
+            'Ik zal onthouden dat software engineering zowel *kunst* als technologie is (, en dat er schoonheid is in code.
+            Ik zal niet vergeten dat programma\'s geschreven moeten worden voor *mensen*, en alleen incidenteel om door machines
+            uitgevoerd te worden.)',
+            'Ik zal onthouden dat de fundamenten dan technologie in de *wetenschap* liggen(, en ik vergeet niet de bijdragen van wetenschappers
+            die mijn kunst steunden; zij die wiskunde, natuurkunde, scheikunde en informatica ontwikkelden.)',
+            'Ik zal mij niet schamen om "Ik weet het niet" te zeggen(, en zal niet nalaten een andere engineer in te schakelen als het nodig is.)',
+            'Wanneer ik door technologie toegang krijg tot persoonlijke gegevens, zal ik de *privacy* respecteren(, en zal de verkregen informatie
+            niet misbruiken.)',
+            'Ik zal onhouden dat, als een engineer, met mijn vermogen de *verplichting* komt( om te scheppen en het leven van mijn medemensen
+            te verbeteren.)',
+        ),
+        I_CC => 'Creative Commons Licentie',
+        I_EPILOGUE0 => 'Als ik deze eed niet breek,
+        
+                        mag het mij gegund zijn te genieten van mijn *leven* en *kunst*,
+                        
+                        *gerespecteerd* zolang ik leef
+                        
+                        en met *genegenheid* herdacht erna.',
+        I_EPILOGUE1 => 'Laat mij de *beste tradities* van mijn roeping bewaken
+                        
+                        en laat mij lang genieten van de vreugde van *innovatie*.
+                        
+                        Geef me het *plezier* om om aan mijn kust
+                        
+                        nieuwe elementen toe te voegen die het *verbeteren*.',
+        I_TAKE      => 'Zweer deze eed',
+        I_SHARE_FB  => 'Deel op Facebook',
+        I_SHARE_TW  => 'Deel op Twitter'
+    );
+?>
