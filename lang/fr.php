0ce5741e85cfd46fd8af61fdafd4656d3fe3c04f
Loic Nageleisen <loic.nageleisen@gmail.com>
2013-12-11 14:19:12 +0100

    french

---
+++ lang/fr.php
@@ -0,0 +1,66 @@
+<?php
+    return array(
+        I_TITLE => 'Serment',
+        I_SWEAR => 'Je jure,',
+        I_INTRO => 'de respecter au meilleur
+        
+                    de mes *capacités* et de mon *jugement*,
+                    
+                    ce serment et cet engagement',
+        I_OATH  => array(
+            'Je respecterai le *savoir* des personnes dans les pas desquelles
+            je marche (ainsi que de ceux qui m\'enseignent et m\'assistent, et
+            de partager avec entrain ce savoir avec ceux qui devront me suivre,
+            tout comme d\'autres l\'ont fait avant moi. Je me rappellerai que
+            *le savoir ne doit pas rester secret* pour ceux qui désirent
+            apprendre.)',
+            'J\'utiliserai la technologie pour le *bien* (et pour le *progrès*
+            de l\'humanité, libre de tout mal, pour l\'amélioration de la vie
+            d\'autrui)',
+            'Je n\'utiliserai pas mon savoir pour un profit injuste(, financier
+            ou d\'un autre genre. Je n\'affirmerai pas que l\'usage d\une
+            technologie soit nécessaire si tel n\'est pas le cas, mais
+            uniquement pour la résolution de problèmes *réels*, libre de toute
+            injustice intentionnelle.)',
+            'Je garderai à l\'esprit que la technologie *s\'améliore*
+            toujours(, et je n\'aurai de cesse d\'apprendre, et le but de ma
+            vie sera de m\'améliorer continûment.)',
+            'Je garderai à l\'esprit qu\'il ya de *l\'art* en l\'ingénierie
+            logicielle ainsi qu\'en la la technologie(, et que beauté existe en
+            le code. Je n\'oublierai pas que les programmes doivent être écrits
+            en premier lieu pour être lus par des *personnes*, et seulement par
+            voie de conséquence pour être exécutés par des machines.)',
+            'Je garderai à l\'esprit que les fondations de la technologie
+            reposent sur les *sciences*(, et je n\'oublierai pas la
+            contribution des scientifiques qui soutenurent mon art; ceux qui
+            développèrent les mathématiques, la physique, la chimie et les
+            sciences de l\'informatique.)',
+            'Je n\'aurai aucune honte à affirmer "Point ne sais-je"(, ni
+            n\'échouerai à quérir un autre ingénieur fût-ce-t-il besoin.)',
+            'Lorsque via la technologie j\'obtiens l\accès à des données
+            privées, je respecterai cette *vie privée*( et ne commettrai pas
+            d\'abus avec les informations disponibles)',
+            'Je garderai à l\'esprit en tant qu\'ingénieur qu\'avec mes
+            capacités et mon pouvoir vient *l\'obligation*( de *créer* et de
+            rendre la vie de mes camarades humains meilleure.)'
+        ),
+        I_CC => 'License Creative Commons',
+        I_EPILOGUE0 => 'Si je ne romps pas ce serment,
+        
+                        qu\'il me soit accordé d\'apprécier *la vie* et *l\'art*,
+                        
+                        *respecté* tant que je vis
+                        
+                        et remémoré avec *affection* par la suite.',
+        I_EPILOGUE1 => 'Puis-je agir de sorte à préserver les *meilleures traditions* de mon métier
+        
+                        et puis-je faire longtemps l\'expérience de la joie de *l\'innovation*.
+                        
+                        Et puis-je faire l\'expérience de la *joie* de donner à mon art
+                        
+                        de nouveaux éléments pour le faire *progresser*.',
+        I_TAKE      => 'Prêter serment',
+        I_SHARE_FB  => 'Partager sur Facebook',
+        I_SHARE_TW  => 'Partager sur Twitter'
+    );
+?>
