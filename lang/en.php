1ea638f55ca3bec1b65182589affa307ecbe6248
Dionysis Zindros <dionyziz@gmail.com>
2014-01-22 04:08:52 +0200

    Move parentheses around punctuation correctly in English

--- lang/en.php
+++ lang/en.php
@@ -12,18 +12,17 @@
             *knowledge must not be kept secret* for those wishing to learn.)',
             'I will use technology for *good* (and for the *progress* of mankind, free of evil, 
             for the betterment of peoples\' lives.)',
-            'I will not use my knowledge for unfair profit(, financial or of other kind. I will not claim that use of technology 
+            'I will not use my knowledge for unfair profit, (financial or of other kind. I will not claim that use of technology 
             is necessary when it is not, but only for the solution of *real* problems, free of all intentional injustice.)',
-            'I will remember that technology always *improves*(, and will never stop learning, and the purpose of my life 
+            'I will remember that technology always *improves*, (and will never stop learning, and the purpose of my life 
             will be continuous *self-improvement*.)',
-            'I will remember that there is *art* to software engineering as well as technology(, and that there is beauty in code. I
+            'I will remember that there is *art* to software engineering as well as technology, (and that there is beauty in code. I
             will not forget that programs must be written for *people* to read, and only incidentally for machines to execute.)',
-            'I will remember that technology foundations lie in *science*(, and I will not forget the contribution of the scientists who supported
+            'I will remember that technology foundations lie in *science*, (and I will not forget the contribution of the scientists who supported
             my art; those who developed mathematics, physics, chemistry and the science of computation.)',
-            'I will not be ashamed to say "I know not"(, nor will I fail to call in another engineer if needed.)',
-            'When through technology I gain access to private data, I will respect *privacy*(
-            and will not abuse the information at hand.)',
-            'I will remember as an engineer that along with my ability and power comes the *obligation*( to
+            'I will not be ashamed to say "I know not", (nor will I fail to call in another engineer if needed.)',
+            'When through technology I gain access to private data, I will respect *privacy* (and will not abuse the information at hand.)',
+            'I will remember as an engineer that along with my ability and power comes the *obligation* (to
             *create* and make the life of my fellow humans better.)'
         ),
         I_CC => 'Creative Commons License',
